201527967cd734569e7f8cf9c9df511b1483b61e
limanling <limanlingcs@gmail.com>
2020-09-21 16:01:18 -0500

    postprocessing

--- system/postprocessing/merge_final.sh
+++ system/postprocessing/merge_final.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E29_oneie_entcoref"
+data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new"
 parent_child_tab_path="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/bak_output_dryrun_E29_ann/parent_children.tab"
 output_ttl=${data_root}"/kb/ttl" 
 # all_merged_ttl=${output_ttl}
@@ -8,13 +8,13 @@ variant=1
 final_ttl=${data_root}"/final_clean"
 
 
-docker run --rm -v ${data_root}:${data_root} -i limanling/uiuc_ie_m36 \
-    /opt/conda/envs/py36/bin/python \
-    /postprocessing/postprocessing_combine_turtle_from_all_sources.py \
-    --root_folder ${data_root} \
-    --final_dir_name 'final' \
-    --output_folder ${output_ttl}
-echo "Final output of English, Russian, Spanish in "${output_ttl}
+# docker run --rm -v ${data_root}:${data_root} -i limanling/uiuc_ie_m36 \
+#     /opt/conda/envs/py36/bin/python \
+#     /postprocessing/postprocessing_combine_turtle_from_all_sources.py \
+#     --root_folder ${data_root} \
+#     --final_dir_name 'final' \
+#     --output_folder ${output_ttl}
+# echo "Final output of English, Russian, Spanish in "${output_ttl}
 
 # # # merge sentiment
 # # docker run --rm -v `pwd`:`pwd` -w `pwd` -i -t limanling/uiuc_ie_m18 \
