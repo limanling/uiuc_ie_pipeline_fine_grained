8260b0ca1a473c19e6f9d9292803d05d1e123d59
limanling <limanlingcs@gmail.com>
2020-09-30 21:04:36 -0500

    cleanKB

--- system/postprocessing/merge_final_vis.sh
+++ system/postprocessing/merge_final_vis.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new"
+data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/vis/WORKING/columbia_vision_shared/cu_graph_merging_ttl/dryrun"
 parent_child_tab_path="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/bak_output_dryrun_E29_ann/parent_children.tab"
-output_ttl=${data_root}"/kb/ttl" 
+output_ttl=${data_root}"/merged_ttl" 
 # all_merged_ttl=${output_ttl}
 variant=1
 final_ttl=${data_root}"/final_clean"
-asr_mapping_file=${data_root}"/final_clean"
 
 
-docker run --rm -v ${data_root}:${data_root} -i limanling/uiuc_ie_m36 \
-    /opt/conda/envs/py36/bin/python \
-    /postprocessing/postprocessing_combine_turtle_from_all_sources.py \
-    --root_folder ${data_root} \
-    --final_dir_name 'final' \
-    --output_folder ${output_ttl}
-echo "Final output of English, Russian, Spanish in "${output_ttl}
+# docker run --rm -v ${data_root}:${data_root} -i limanling/uiuc_ie_m36 \
+#     /opt/conda/envs/py36/bin/python \
+#     /postprocessing/postprocessing_combine_turtle_from_all_sources.py \
+#     --root_folder ${data_root} \
+#     --final_dir_name 'final' \
+#     --output_folder ${output_ttl}
+# echo "Final output of English, Russian, Spanish in "${output_ttl}
 
 # # # merge sentiment
 # # docker run --rm -v `pwd`:`pwd` -w `pwd` -i -t limanling/uiuc_ie_m18 \
@@ -35,7 +34,6 @@ docker run --rm -v ${data_root}:${data_root} -v ${parent_child_tab_path}:${paren
     ${data_root}/cleankb.param ${output_ttl} ${final_ttl} ${variant} \
     --parent_child_tab_path ${parent_child_tab_path} \
     --eval m36
-        # --asr_mapping ${asr_mapping_file} 
 docker run --rm -v ${data_root}:/aida-tools-java11/sample_params/m36-eval/${data_root} \
     -v ${parent_child_tab_path}:/aida-tools-java11/sample_params/m36-eval/${parent_child_tab_path} \
     -w /aida-tools-java11 -i -t limanling/aida-tools \
---
+++ system/postprocessing/py_validator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# https://github.com/NextCenturyCorporation/AIDA-Interchange-Format/blob/ef8beffc50d05466be730fd3c67330d6dfbdeaf3/python/tests/py_validator.sh
+
+# if a command returns a non zero return value, exit script
+set -e
+
+# run examples.py
+# DIR_PATH is the directory where example files are to be written
+DIR_PATH=$1
+
+# # get current time
+# test_directory=${DIR_PATH}_`date +%s`
+# mkdir $test_directory
+
+# validate test file directory
+# if any files are invalid, the script will end (see above)
+docker run --rm -it \
+       -v ${DIR_PATH}:/v \
+       --entrypoint /opt/aif-validator/java/target/appassembler/bin/validateAIF \
+       nextcenturycorp/aif_validator:latest \
+       -o --ont /opt/aif-validator/java/src/main/resources/com/ncc/aif/ontologies/LDCOntologyM36 -d /v
+
+# # if all files are valid, delete the test file directory
+# rm -r $test_directory
