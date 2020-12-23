7d055215400dcf1008d3ea3545f80a44fc1c53d2
limanling <limanlingcs@gmail.com>
2020-12-23 03:21:05 -0600

    update merging

--- system/postprocessing/merge_final.sh
+++ system/postprocessing/merge_final.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/env bash
 
-data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new"
-# data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E29_oneie_entcoref"
-parent_child_tab_path="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/bak_output_dryrun_E29_ann/parent_children.tab"
-output_ttl=${data_root}"/kb/ttl" 
-cu_merged_ttl=${data_root}"/merged_ttl_cu"
-# all_merged_ttl=${output_ttl}
+data_root="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210"
+parent_child_tab_path="/shared/nas/data/m1/AIDA_Data/LDC_raw_data/LDC2020E32_AIDA_TAC_SM-KBP_2020_Evaluation_Source_Data/docs/parent_children.tab"
+ltf_dir="/shared/nas/data/m1/AIDA_Data/LDC_raw_data/LDC2020E32_AIDA_TAC_SM-KBP_2020_Evaluation_Source_Data/data/ltf/ltf"
+
+output_ttl=${data_root}"/merged_ttl_eval01_coref" 
+output_ttl_test=${data_root}"/merged_ttl_eval01_clean_coref"
+cu_merged_ttl=${data_root}"/merged_ttl_cu_vision_sent_coref"
 variant=1
-final_ttl=${data_root}"/final_clean"
-# asr_mapping_file=${data_root}"/final_clean"
+final_ttl=${data_root}"/merged_all_clean_coref"
+final_ttl_str=${data_root}"/merged_all_clean_coref_str"
+final_ttl_str_fix=${data_root}"/merged_all_clean_coref"
 
-sentiment_dir="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment"
-relation_cs_file_en="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment/en.multi_sent.sponsor_assignblame.cs"
-relation_cs_file_ru="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment/ru.multi_sent.sponsor_assignblame.cs"
-relation_cs_file_es="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment/es.multi_sent.sponsor_assignblame.cs"
+# output_ttl=${data_root}"/merged_ttl_eval01" 
+# output_ttl_test=${data_root}"/merged_ttl_eval01_clean"
+# cu_merged_ttl=${data_root}"/merged_ttl_cu_vision_sent"
+# variant=1
+# final_ttl=${data_root}"/merged_all_clean"
+# final_ttl_str=${data_root}"/merged_all_clean_str"
+# # final_ttl_str_fix=${data_root}"/merged_all_clean_str_fix"
 
-evt_source_tab_en="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment/en.event_sources.cs"
-evt_source_tab_ru="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment/ru.event_sources.cs"
-evt_source_tab_es="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E11_new/cu_sentiment/es.event_sources.cs"
+sentiment_dir="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment"
+relation_cs_file_en="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment/en.multi_sent.sponsor_assignblame.cs"
+relation_cs_file_ru="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment/ru.multi_sent.sponsor_assignblame.cs"
+relation_cs_file_es="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment/es.multi_sent.sponsor_assignblame.cs"
+evt_source_tab_en="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment/en.event_sources.cs"
+evt_source_tab_ru="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment/ru.event_sources.cs"
+evt_source_tab_es="/shared/nas/data/m1/manling2/aida_docker_test/uiuc_ie_pipeline_fine_grained/output/output_dryrun_E32_gpu_1210/cu_sentiment/es.event_sources.cs"
+
+docker run --rm -v ${data_root}:${data_root} -v ${parent_child_tab_path}:${parent_child_tab_path} -i limanling/uiuc_ie_m36 \
+    /opt/conda/envs/py36/bin/python \
+    /postprocessing/postprocessing_cleankb_params_caci.py \
+    ${data_root}/cleankb.param ${output_ttl} ${output_ttl_test} ${variant} \
+    --parent_child_tab_path ${parent_child_tab_path} \
+    --eval m36
+docker run --rm -v ${data_root}:${data_root} \
+    -v ${parent_child_tab_path}:${parent_child_tab_path} \
+    -w /aida-tools-java11 -i -t limanling/aida-tools \
+    /aida-tools-java11/aida-eval-tools/target/appassembler/bin/cleanKB  \
+    ${data_root}/cleankb.param
 
 # merge sentiment
 for thread_id in {0..9}
@@ -48,4 +69,14 @@ docker run --rm -v ${data_root}:${data_root} \
     /aida-tools-java11/aida-eval-tools/target/appassembler/bin/cleanKB  \
     ${data_root}/cleankb.param
 
-
+for thread_id in {0..19}
+do
+    docker run --rm -v ${final_ttl}:${final_ttl} -v ${final_ttl_str}:${final_ttl_str} -v ${ltf_dir}:${ltf_dir} -i limanling/uiuc_ie_m36 \
+        /opt/conda/envs/py36/bin/python \
+        /postprocessing/aif_addstr.py \
+        --input_ttl_folder ${final_ttl} \
+        --output_ttl_dir ${final_ttl_str} \
+        --ltf_dir ${ltf_dir} \
+        --thread 20 --current_thread ${thread_id} &
+done
+wait
